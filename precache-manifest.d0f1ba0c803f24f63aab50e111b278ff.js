ab2c3ef881e5d0b714a14f2f41bed788e5f27e5a
Rajat <rajat.gupta@mail.com>
2022-03-06 00:27:58 +0530

    Updates

---
+++ precache-manifest.d0f1ba0c803f24f63aab50e111b278ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947001a26cb52756ad8a5836db73a50f",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "3f9a4ea362a1a2a28c08",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "3a93af7ede7143059ce6",
+    "url": "/home/static/js/2.95903239.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.95903239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9a4ea362a1a2a28c08",
+    "url": "/home/static/js/main.51a71bd5.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "098e26b9ae4ba19dccd60219fc9d7e82",
+    "url": "/home/static/media/rajatg.098e26b9.png"
+  },
+  {
+    "revision": "ad5d07156cb1c5e9ae24d80f4b14bbad",
+    "url": "/home/static/media/resume.ad5d0715.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.d94a0c528241756985feadabe6d8d050.js"
+  "/home/precache-manifest.d0f1ba0c803f24f63aab50e111b278ff.js"
 );
 
 self.addEventListener('message', (event) => {
